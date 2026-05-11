994e73b35cd0550b38e88a1eb3f619cc3c4299be
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-11 12:05:39 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,11 @@
 # Replace the "ANSWER HERE" for your answer
+from google.genai.types import GenerationConfigRoutingConfig
+
 
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+
+    meses=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+
+    if month > 12 or month < 1:
+        return "error"
+    return meses[month -1]
